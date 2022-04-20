7ed78ce0a3d4a39451e34dda804539aee5522702
dongho <backack2@gmail.com>
2022-04-20 22:55:13 +0900

    JPA

---
+++ src/test/java/com/dh/micro/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.dh.micro.springboot.web.domain.posts;
+
+import com.dh.micro.springboot.domain.posts.PostRepository;
+import com.dh.micro.springboot.domain.posts.Posts;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("dh@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
