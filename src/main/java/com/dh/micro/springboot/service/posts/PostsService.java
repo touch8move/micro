7cad617975e3fa92c56035dd63902ecc4505d1f7
dongho <backack2@gmail.com>
2022-05-04 06:35:40 +0900

    게시글 조회

--- src/main/java/com/dh/micro/springboot/service/posts/PostsService.java
+++ src/main/java/com/dh/micro/springboot/service/posts/PostsService.java
@@ -1,6 +1,7 @@
 package com.dh.micro.springboot.service.posts;
 
 import com.dh.micro.springboot.domain.posts.Posts;
+import com.dh.micro.springboot.web.dto.PostsListResponseDto;
 import com.dh.micro.springboot.web.dto.PostsResponseDto;
 import com.dh.micro.springboot.web.dto.PostsSaveRequestDto;
 import com.dh.micro.springboot.domain.posts.PostsRepository;
@@ -8,6 +9,9 @@ import com.dh.micro.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,10 @@ public class PostsService {
     public PostsResponseDto findById (Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/dh/micro/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.dh.micro.springboot.web.dto;
+
+import com.dh.micro.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
